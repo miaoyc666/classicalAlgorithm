d329598dc6b0df27fae4fef7792d71437b21078e
miaoyc <miaoyc1989@hotmail.com>
2022-01-27 23:56:40 +0800

    ====feat: update heap sort====

--- sort/heap_sort.py
+++ sort/heap_sort.py
@@ -13,3 +13,17 @@ Description  : 堆排序
 1.把无序数组构建成二叉堆，需要从小到大（或从大到小）排序，则构建成最大堆（或最小堆）；
 2.循环删除堆顶元素，替换到二叉堆的末尾，调整堆产生新的堆顶。
 """
+
+
+def heap_sort():
+    pass
+
+
+def down_adjust():
+    pass
+
+
+if __name__ == '__main__':
+    input_nums = [13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11]
+    print(input_nums)
+    print(heap_sort(input_nums))
